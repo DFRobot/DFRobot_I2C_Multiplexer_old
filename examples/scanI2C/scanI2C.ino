2e88c97ce25b09e921241a7a622c4382695ba1b2
mengbishu <841616394@qq.com>
2018-05-10 13:52:30 +0800

    v0.1

--- examples/scanI2C/scanI2C.ino
+++ examples/scanI2C/scanI2C.ino
@@ -1,3 +1,15 @@
+ /*!
+  * file communicate.ino
+  * 
+  * Connect I2C devices to I2CMultiplexer, and connect I2CMultiplexer to Leonardo, then download this example
+  * @n open serial monitor to check the data.
+  *
+  * Copyright   [DFRobot](http://www.dfrobot.com), 2016
+  * Copyright   GNU Lesser General Public License
+  *
+  * version  V0.1
+  * date  2018-5-10
+  */
 #include <DFRobot_I2CMultiplexer.h>
 
 DFRobot_I2CMultiplexer I2CMultiplexer(0x70);
